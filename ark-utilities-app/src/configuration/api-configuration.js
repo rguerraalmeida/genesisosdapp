4a08a1235d4ea1d9e90ec09f832f11b2537ded34
Rui Guerra <r.guerra.almeida@gmail.com>
2020-05-11 19:01:07 +0100

    added config files and api service

---
+++ ark-utilities-app/src/configuration/api-configuration.js
@@ -0,0 +1,6 @@
+// class Configuration {
+//     API_BASE_PATH_x = 'https://localhost:44371';
+//}
+//   export default Configuration;
+
+export const API_BASE_PATH = 'https://localhost:44371';
---
+++ ark-utilities-app/src/services/arkutilities-api.js
@@ -0,0 +1,21 @@
+// import configuration, { API_BASE_PATH } from '../configuration/api-configuration';
+
+// //export const apiRestPath = 'api/arkutilities';    
+
+// class arkutilitiesApi
+// {
+//     constructor() {
+//       this.servicePath = API_BASE_PATH + 'api/arkutilities'; //apiRestPath;
+//     }
+
+//     getLostDinos() {
+//        fetch(this.servicePath)
+//         .then(response => response.json());
+
+//       return response;
+//     }
+
+// }
+
+
+// export default arkutilitiesApi;
