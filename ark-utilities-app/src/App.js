7239a54308c1bc11a77ee04a9be356c5c8c88e11
Rui Guerra <r.guerra.almeida@gmail.com>
2020-05-14 04:35:46 +0100

    changed app icon to reflect game icon, changeds app title,  added search description so users can know whats columns the expression is being searched for

--- ark-utilities-app/src/App.js
+++ ark-utilities-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import SearchPanel from './components/search-panel/search-panel';
@@ -35,29 +34,6 @@ function App() {
 
         </div>
       </div>
-
-     
-      
-     
-
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-
-   
-
     </div>
   );
 }
